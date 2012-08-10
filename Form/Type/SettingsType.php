6e741da0cdf353bf72e554dda4f360f4c055313b
gnal <qwertypianox@gmail.com>
2012-08-10 16:53:39 -0400

    fix

--- Form/Type/SettingsType.php
+++ Form/Type/SettingsType.php
@@ -3,26 +3,19 @@
 namespace Msi\Bundle\BlockBundle\Form\Type;
 
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\FormBuilder;
+use Symfony\Component\Form\FormBuilderInterface;
 
 class SettingsType extends AbstractType
 {
-    public function buildForm(FormBuilder $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options)
     {
         foreach ($options['fields'] as $field) {
             $builder->add($field[0], $field[1], $field[2]);
         }
     }
 
-    public function getDefaultOptions(array $options)
-    {
-        return array(
-            'fields' => array(),
-        );
-    }
-
     public function getName()
     {
-        return 'settings';
+        return 'msi_block_settings';
     }
 }
